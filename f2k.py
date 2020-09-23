f3f45138007106d1ed625fbc315b094329b03d0f
GoswamiHemant <70230295+GoswamiHemant@users.noreply.github.com>
2020-09-22 22:41:33 -0400

    Delete f2k.py

--- f2k.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# https://github.com/GoswamiHemant/bch5884
-
-F=int(input("Please enter temperature in Fahrenheit: "))
-K=(F+459.67)/1.8
-print("Temperature in Kelvin is", K)
