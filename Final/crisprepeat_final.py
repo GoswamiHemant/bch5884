24028dae62ffd0de1056a2f8216edf8c160baae3
GoswamiHemant <70230295+GoswamiHemant@users.noreply.github.com>
2020-12-07 00:24:02 -0500

    Update crisprepeat_final.py

--- Final/crisprepeat_final.py
+++ Final/crisprepeat_final.py
@@ -40,7 +40,7 @@ for length in length_list:
     print("\n\n The",length, "nucleotide long repeat sequence in the given DNA sequence is")
     print(sequence)
     plt.bar(*zip(*sequence.copy().items()), color='orange', width=0.3,)
-    plt.title('Occurrence of 21 nucleotide long repeats in S. sulfotaricus')
+    plt.title('Occurrence of 21 nucleotide long repeats in S. sulfataricus')
     plt.xlabel('Unique repeats', color='green', fontsize=16)
     plt.ylabel('Frequency', color='green', fontsize=16)
     plt.savefig("21ntdrepeats.png", format="png")
@@ -65,13 +65,13 @@ for length in length_list:
     print("\n\n The",length, "nucleotide long repeat sequence in the given DNA sequence is")
     print(sequence)
     plt.bar(*zip(*sequence.copy().items()), color='purple', width=0.3,)
-    plt.title('Occurrence of 22 nucleotide long repeats in S. sulfotaricus')
+    plt.title('Occurrence of 22 nucleotide long repeats in S. sulfataricus')
     plt.xlabel('Unique repeats', color='green', fontsize=16)
     plt.ylabel('Frequency', color='green', fontsize=16)
     plt.savefig("22ntdrepeats.png", format="png")
     plt.show()
 
-#Finding 22 nucleotides long repeat sequences and their frequency  
+#Finding 23 nucleotides long repeat sequences and their frequency  
 length_list = [23]
 
 for length in length_list:
@@ -90,7 +90,7 @@ for length in length_list:
     print("\n\nThe",length, "nucleotide long repeat sequence in the given DNA sequence is")
     print(sequence)
     plt.bar(*zip(*sequence.copy().items()), color='maroon', width=0.1,)
-    plt.title('Occurrence of 23 nucleotide long repeats in S. sulfotaricus')
+    plt.title('Occurrence of 23 nucleotide long repeats in S. sulfataricus')
     plt.xlabel('Unique repeats', color='green', fontsize=16)
     plt.ylabel('Frequency', color='green', fontsize=16)
     plt.savefig("23ntdrepeats.png", format="png")
@@ -106,11 +106,11 @@ def writehtml():
 	f.write("<head>")
 	f.write("<style> body { background:lightblue; } \n")
 	f.write("</style>\n")
-	f.write("<h1> CRISPR repeat analysis in Sulfolobus sulfotaricus genomic DNA sequence </h1>\n")
+	f.write("<h1> CRISPR repeat analysis in sulfolobus sulfataricus genomic DNA sequence </h1>\n")
 	f.write("</head>")
 	f.write("<body>\n")
 	f.write("<p>  My research involves the structural and biochemical cjaracterization of  of novel type V CRISPR-Cas proteins. CRISPR stands for Clustered Regularly Interspaced Short Palindromic Sequence. An evolutionalary struggle of billions of years between prokaryotes and viruses have led to the development of adaptive immune system in bacteria. CRISPR-Cas based immunity is found in 90% of archaea and 50% of bacteria. Based on the CRISPR-Cas loci, CRISPR Cas systems have been classified into six distinct types and each uses a set of special Cas proteins for DNA interference. Type I and type III CRIPPR systems employs a multi-protein complex for crRNA binding and DNA cleavage, while type II (Cas9) and type V CRISPR (Cas12) systems utilize a single protein, that recognizes its crRNA and cleaves the target DNA. Cas9 protein uses a distinct domain, HNH or RuvC, to cleave each strand of DNA while Cas12 uses just RuvC domain to cleave both the strands of DNA. </p>\n")
-	f.write("<p> Recently I found that Sulfolobus sulfotaricus contains DNA sequesnce similar to Cas12 genes. So to verify if these sequences are really Cas protein genes, we want to identify the CRISPR repeats downstream of promising Cas genes because these repeats are an integral part of CRISPR-Cas array. </p>\n")
+	f.write("<p> Recently I found that Sulfolobus sulfataricus contains DNA sequesnce similar to Cas12 genes. So to verify if these sequences are really Cas protein genes, we want to identify the CRISPR repeats downstream of promising Cas genes because these repeats are an integral part of CRISPR-Cas array. </p>\n")
 	f.write('<p> <img src="21ntdrepeats.png"> </p>')
 	f.write('<p> <img src="22ntdrepeats.png"> </p>')
 	f.write('<p> <img src="23ntdrepeats.png"> </p>')
